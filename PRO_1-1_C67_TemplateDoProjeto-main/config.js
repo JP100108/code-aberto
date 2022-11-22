79794de7b2aba4a69ddda5878d1502f25a44ece2
JP100108 <100318527+JP100108@users.noreply.github.com>
2022-11-22 18:33:05 -0300

    Update config.js

--- PRO_1-1_C67_TemplateDoProjeto-main/config.js
+++ PRO_1-1_C67_TemplateDoProjeto-main/config.js
@@ -1,9 +1,17 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB-i2qNmWJNpfPaU6BXaKCbb3u51N-V2lM",
+  authDomain: "code-aberto.firebaseapp.com",
+  projectId: "code-aberto",
+  storageBucket: "code-aberto.appspot.com",
+  messagingSenderId: "34500503886",
+  appId: "1:34500503886:web:d112a1d2f273b1be66d61f"
 };
+
+
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
 
